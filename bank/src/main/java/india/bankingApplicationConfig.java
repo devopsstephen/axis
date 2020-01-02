c24c0c082c2955b27ca7cba0d4d4a852a8425f5d
devopsstephen <devopsstephen@gmail.com>
2020-01-02 10:21:05 +0530

    created bank project

---
+++ bank/src/main/java/india/bankingApplicationConfig.java
@@ -0,0 +1,41 @@
+package india;
+
+import org.linkki.framework.state.ApplicationConfig;
+import org.linkki.framework.ui.application.ApplicationFooter;
+import org.linkki.framework.ui.application.ApplicationHeader;
+import org.linkki.framework.ui.application.menu.ApplicationMenuItemDefinition;
+import org.linkki.util.Sequence;
+
+/**
+ * An {@link ApplicationConfig} using the default {@link ApplicationHeader
+ * application header} and {@link ApplicationFooter application footer}.
+ */
+public class bankingApplicationConfig implements ApplicationConfig {
+
+    @Override
+    public String getApplicationName() {
+        return "banking Sample Application";
+    }
+
+    @Override
+    public String getApplicationVersion() {
+        return "0.0.1-SNAPSHOT";
+    }
+
+    @Override
+    public String getApplicationDescription() {
+        return "banking Sample Application";
+
+    }
+
+    @Override
+    public String getCopyright() {
+        return "";
+    }
+
+    @Override
+    public Sequence<ApplicationMenuItemDefinition> getMenuItemDefinitions() {
+        return Sequence.empty();
+    }
+
+}
